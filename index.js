c8709db4add495e1eb463d322d5535680a496cf7
karthik <karthik@edulab.in>
2022-12-27 11:37:02 +0530

    kg-feat-mongodb-connectionNode

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 
 
-const {MongoClient}=require('mongodb')
-const url='mongodb://localhost:27017'
-const client=new MongoClient(url);
-const database='e-com'
+const dbConnect=require('./mongodb')
 
 
-async function getData(){
-    let result=await client.connect();
-    let db=result.db(database);
-    let collection=db.collection('products')
-    let response=await collection.find().toArray();
-    console.log(response)
+// async function getData(){
+//     let result=await client.connect();
+//     let db=result.db(database);
+//     let collection=db.collection('products')
+//     let response=await collection.find().toArray();
+//     console.log(response)
+// }
+
+// getData()
+
+
+const main=async()=>{
+    let data=await dbConnect();
+       data=await data.find().toArray();
+       console.log(data)
 }
+main();
+      
 
-getData()
 
- 
+       //another way
+// dbConnect().then((res)=>{
+//    res.find().toArray().then((data)=>{
+//     console.log(data)
+//    })
+// })
